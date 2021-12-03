ff5a8867e9c24a5fdd91ab8f757d416f25b0426e
thom laso <thomlinpjlaso@gmail.com>
2021-12-03 20:15:47 +0530

    adding the repo plastichoiuseware
     adding the repo plastichoiusewar and all its sub repo

---
+++ index2.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<htnl>
+    <head>
+        <title>index</title>
+        <link rel="stylesheet" href="index2.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"><!--resposive for different size-->
+    </head>
+    <body>
+
+		
+            <ul>
+            <li><img src="cart-59-48.png" href="#cart" id="cart"style="position: relative;top:50px; right: 10px;"></a></li>
+                <li><div class="dropdown"style="position: relative;top:50px;right: 15px;">
+                <button class="dropbtn">Products</button>
+                    <div class="dropdown-content">
+                        <a href="householdplastics.html">Household Products</a>
+                        <a href="#">Furniture Plastics</a>
+                        <a href="#">Fishing Tackle</a>
+                        <a href="#">Kitchen Electronics</a>
+                    </div></li>
+                <li><a class="active" href="#home"style="position: relative;top:50px;right: 15px;">Home</a></li>
+                <li><input type="text" id="search" name="search"placeholder="Search Produtcs" style="position: relative;top:50px;right: 15px;">
+                <li>
+                    <div id='button-search'>
+                        <input type="image" img src='magnifying-glass-40x40.png' style="position: relative;top:50px;right: 15px;">
+                    </div>
+                </li>
+                <img src="LogoMakr.png" id="logo">
+            </ul>
+  
+
+
+    <div class="indHPlastic"style="position: relative;top:150px; left:500px">
+        <a href="householdplastics.html">
+            <img src="HomeImage/housejoldPlastic.png" id="inHouseP">
+        </a>
+    <!--<div class="grid-item" style="background: rgb(250, 235, 103);">2</div>
+        <div class="grid-item" style="background: rgb(133, 253, 109);">3</div>
+        <div class="grid-item" style="background: rgb(137, 225, 247);">4</div>
+        <div class="grid-item" style="background: rgb(100, 98, 209);">5</div>
+        <div class="grid-item" style="background: rgb(250, 111, 204);">6</div>-->
+    </div>
+    
+    <div class="indFPlastic"style="position: relative;top:150px; left:500px">
+        <a href="default.asp">
+            <img src="HomeImage/furnitureHome.png" id="inHouseF">
+        </a>
+    <!--<div class="grid-item" style="background: rgb(250, 235, 103);">2</div>
+        <div class="grid-item" style="background: rgb(133, 253, 109);">3</div>
+        <div class="grid-item" style="background: rgb(137, 225, 247);">4</div>
+        <div class="grid-item" style="background: rgb(100, 98, 209);">5</div>
+        <div class="grid-item" style="background: rgb(250, 111, 204);">6</div>-->
+    </div>
+        
+    <div class="indWrdbPlastic"style="position: relative;top:150px; left:500px">
+        <a href="default.asp">
+            <img src="HomeImage/wardrobe.png" id="inHouseWrdb">
+        </a>
+    <!--<div class="grid-item" style="background: rgb(250, 235, 103);">2</div>
+        <div class="grid-item" style="background: rgb(133, 253, 109);">3</div>
+        <div class="grid-item" style="background: rgb(137, 225, 247);">4</div>
+        <div class="grid-item" style="background: rgb(100, 98, 209);">5</div>
+        <div class="grid-item" style="background: rgb(250, 111, 204);">6</div>-->
+    </div>
+    </body>
+</html>
+<?php
+                    $search = $_POST['search'];
+                    $search = "www.google.com";//THIS WOULD WORK, BUT I WAS SHOWING HOW TO USE FORM
+                    
+                    //IF WORD FOUND IN HOME PAGE
+                    if (stripos($home, $search) !== false) {//USING EXAMPLE.COM TO SHOW IT WORKS
+                        echo '<a href="'.$homePageName.'">'.$homePageName.'</a>';
+                    }
+                    ?>
+<!--style = "position:relative; left:950px; top:20px;"-->
