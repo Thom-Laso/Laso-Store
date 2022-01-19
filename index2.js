abea79992091bcd5836237300b3fa684f15ef36e
Thom-Laso <thomlinpjlaso.com>
2022-01-19 18:22:48 +0530

    modifying index2.js

--- index2.js
+++ index2.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 var counter=1;
 setInterval(function(){
@@ -8,14 +8,5 @@ setInterval(function(){
         counter = 1;
     }
 }, 5000);
-=======
 
-var counter=1;
-setInterval(function(){
-    document.getElementById('radio'+counter).checked=true;
-    counter++;
-    if(counter > 4){
-        counter = 1;
-    }
-}, 5000);
->>>>>>> 2498562ce3a36ed5955783b4e59bbfef2a942944
+
